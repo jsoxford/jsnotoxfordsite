76184ccc013e87338d3173fd50662d72d91b9822
Gabor Javorszky <gabor@javorszky.co.uk>
2021-01-13 13:03:30 +0000

    remove twitter link / logo from footer, adjust metadata

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "JSNotOxford",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed, and social share preview images.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Small page for JSNotOxford on 13th January 2021",
+  authorName: "Gabor Javorszky",
+  twitterUsername: "javorszky", // no `@`
 };
