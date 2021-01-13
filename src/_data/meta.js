64ab50d5536d055fd12fc3184e711c4ca4a7d44d
Gabor Javorszky <gabor@javorszky.co.uk>
2021-01-13 13:17:28 +0000

    added site content

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   siteName: "JSNotOxford",
   siteDescription:
-    "Small page for JSNotOxford on 13th January 2021",
+    "Tiny online unconference on 13th January 2021",
   authorName: "Gabor Javorszky",
   twitterUsername: "javorszky", // no `@`
 };
