cfb056298f989d568f09cdc3e1cc70bb87a130f7
Max Glenister <moglenstar@gmail.com>
2021-02-15 16:39:43 +0000

    Tidy up meta tags, made titles useful, removed useless background size style

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: "https://not.jsoxford.com/",
-  siteName: "MaybeNotJSOxford",
+  siteName: "JSNotOxford",
   siteDescription: "A monthly catchup about tech (maybe JS), that's online (not in Oxford)",
   authorName: "JS Oxford",
   twitterUsername: "JSOxford", // no `@`
